a350130ddde36a386ac7f1b2d1d82bb341c0e73e
Ricardo Gama <gamaramosr@gmail.com>
2025-05-13 10:15:35 -0600

    Se agrega estructura base para proyecto empleados- prueba tecnica

---
+++ src/main/java/com/babel/test/BabelTestApplication.java
@@ -0,0 +1,13 @@
+package com.babel.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelTestApplication.class, args);
+	}
+
+}
